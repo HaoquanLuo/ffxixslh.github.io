0724f694d5d0afd4228daad8ad252432bd7710ff
luohao <1223048059@qq.com>
2023-03-19 20:11:52 +0800

    chore: document.title changed

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const rootElement = document.getElementById('app')
 const helloElement = document.createElement('div')
 helloElement.innerHTML = `Hello! I'm ffxixslh`
 rootElement.appendChild(helloElement)
+
+function initialize() {
+  document.title = `ffxixslh's Blog`
+}
+
+initialize()
